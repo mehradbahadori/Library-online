d91c6dbbea8014e487421f460366d7a9b316261c
mehradbahadori <alireza.bahadorizade9@gmail.com>
2021-11-28 23:25:58 +0330

    BookRental Class has been fixed

--- src/main/java/ir/mehrad/library/rentBook/BookRental.java
+++ src/main/java/ir/mehrad/library/rentBook/BookRental.java
@@ -2,17 +2,28 @@ package ir.mehrad.library.rentBook;
 
 import ir.mehrad.library.book.Book;
 import ir.mehrad.library.user.User;
-import org.springframework.data.annotation.Id;
 
+import javax.persistence.*;
+
+@Entity
 public class BookRental {
     @Id
-    private String id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
     private String rentDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
     private Book book;
+
+    @ManyToOne(fetch = FetchType.LAZY)
     private User user;
 
-    public String getId() {
+    public long getId() {
         return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 
     public String getRentDate() {
@@ -33,10 +44,6 @@ public class BookRental {
 
     public User getUser() {
         return user;
-    }
-
-    public void setId(String id) {
-        this.id = id;
     }
 
     public void setUser(User user) {
